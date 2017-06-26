82792d24c0b3c488906b75f27a90c974359b665a
vaishnavibadam <vittu512@gmail.com>
2017-06-26 13:54:21 +0530

    sending disk usage warning to mail

--- diskusage_warning.sh
+++ diskusage_warning.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-#df -T| awk '{print $3}' |cat >disk|awk 'BEGIN { n = 0; } {for ( i = 2 ; i <= NF ; i++ ) $n+ = $disk[i]; } END { print $n; }'
+echo "Your is your disk usage is:"|cat >>disk_usage.txt
+df / | grep / | awk '{ print $5}'|cat >>disk_usage.txt #for printing disk_usage to disk_usage.txt file 
 
-echo df -T| awk '{print $5}' | sed 's/%//g'
+echo "MAILTO=vittu512@gmail.com
+59 14 * * * bash /home/rihu/assignment/diskusage_warning.sh 
+0 15 * * *  cat /home/rihu/assignment/disk_usage.txt
+1 15 * * * rm disk_usage.txt"  | crontab - #scheduling execution of diskusage_warning.sh,printing disk_usage.txt to mail and removing disk_usage.txt file   
+
+
