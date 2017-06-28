1d6b21a832dd6d66f0686deda91b8f940055519c
vaishnavibadam <vittu512@gmail.com>
2017-06-28 19:01:50 +0530

    Update remotemachine_fileslist.sh

--- remotemachine_fileslist.sh
+++ remotemachine_fileslist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -le 2 ]];
-then              #checking whether input has 2 arguments 
+then              #checking whether input has 3 arguments 
 	echo "usage:$0 <username> <ip address/hostname> <password> ['command'] [location]"
 	exit 1
 fi 
@@ -9,7 +9,7 @@ if ! [[ $2 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then  #valida
 	echo "usage:$0 <username> <ip address>"
 	exit
 fi
-#ssh $1@$2 ls -R|xargs -n 1 basename   #connecting to remote system and listing files
+ #connecting to remote system and listing files
 if [ $# -gt 2 ]
 then
 sshpass -p $3 ssh $1@$2 $4 $5
